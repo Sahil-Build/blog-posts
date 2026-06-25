f108ab09891d185943fed71abfd6ce712c64cf1b
S. SAHIL <sahilshaik2849@gmail.com>
2026-06-25 13:21:36 +0530

    Use dynamic port for deployment

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ let posts = [
   },
 ];
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
